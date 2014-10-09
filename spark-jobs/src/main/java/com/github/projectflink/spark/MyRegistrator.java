bacb325584e01256a45a5c95762b9993fc4a2d96
Robert Metzger <rmetzger@apache.org>
2014-10-09 13:55:25 +0200

    fix

--- spark-jobs/src/main/java/com/github/projectflink/spark/MyRegistrator.java
+++ spark-jobs/src/main/java/com/github/projectflink/spark/MyRegistrator.java
@@ -1,21 +1,13 @@
 package com.github.projectflink.spark;
 
 import com.esotericsoftware.kryo.Kryo;
-import org.apache.spark.SparkConf;
-import org.apache.spark.serializer.KryoSerializer;
+import org.apache.spark.serializer.KryoRegistrator;
 
 /**
 * Created by robert on 10/9/14.
 */
 
-public class MyRegistrator extends KryoSerializer {
-	public MyRegistrator() {
-		super();
-	}
-
-	public MyRegistrator(SparkConf conf) {
-		super(conf);
-	}
+public class MyRegistrator implements KryoRegistrator {
 	public void registerClasses(Kryo kryo) {
 		System.err.println("Registered Point with Kryo");
 		kryo.register(KMeansArbitraryDimension.Point.class);
