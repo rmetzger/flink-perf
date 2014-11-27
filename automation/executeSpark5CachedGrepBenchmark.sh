8b823a456d5e0881e6d98f9f37fd425941c43efe
Robert Metzger (GCloud) <rmetzger@apache.org>
2014-11-27 10:32:10 +0000

    pre cluster shutdown

--- automation/executeSpark5CachedGrepBenchmark.sh
+++ automation/executeSpark5CachedGrepBenchmark.sh
@@ -1,6 +1,7 @@
-for i in 1 2 3; do
+export SIZE=$1
+for i in 1 2 3 4 5; do
 	echo "i = $i"
-	echo "running spark cached grep $i" >> log_spark_5_cached_grep_driver
+	echo "running spark cached grep $i with $SIZE in exec" >> log_spark_5_cached_grep_driver
 	start1=`date +%s`
 	./runSparkCachedGrep.sh tree house garden nomatchterm soda >> log_spark_5_cached_grep_driver
 	end=`date +%s`
--- automation/runSparkPagerank.sh
+++ automation/runSparkPagerank.sh
@@ -6,8 +6,7 @@ echo "Running Spark PR example"
 
 $SPARK_HOME/bin/spark-submit --master $SPARK_MASTER $SPARK_YARN \
  --class com.github.projectflink.spark.Pagerank \
- --driver-memory 8G \
  `ls "$TESTJOB_HOME"/spark-jobs/target/spark-jobs-*-All.jar` \
- $SPARK_MASTER 41652230 0.5 80 $HDFS_PAGERANK_OUT $HDFS_PAGERANK_IN $DOP
+ $SPARK_MASTER 41652230 0.5 30 $HDFS_PAGERANK_OUT $HDFS_PAGERANK_IN $DOP
 
 
